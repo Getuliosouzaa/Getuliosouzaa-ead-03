5b1e1192ecd2e33eb4911349330648867d0812f9
Getulio souza <getuliocone@gmail.com>
2018-04-03 17:48:50 -0300

    EAD-03

---
+++ funcoes/js/script.js
@@ -0,0 +1,26 @@
+function media (n1,n2){
+    var n1;
+    var n2;
+    var media = (n1 + n2) /2;
+
+    console.log("A Media é: " + media);
+}
+
+function escrreverNumerosAte(ate){
+    var ate;
+
+    for(var i = 1; i<= ate; i++){
+        console.log(i);
+    }
+}
+
+function divisiveisPor(x,y){
+    var x;
+    var y;
+
+    for(var i=x; i<=y; i++ ){
+        console.log(i)
+        }
+}
+  
+
